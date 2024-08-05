0c9664fe38e1e8803af1210608bee716016b0e67
Xav86 <gustavo.g.2005@gmail.com>
2024-08-05 10:43:54 -0300

    visto tipos string, number e boolean no Ts

---
+++ 002 - Tipos/001 - String - Number - Boolean/index.ts
@@ -0,0 +1,14 @@
+let pedido: string = 'Pizza';
+pedido = 'teste legal';
+
+let numero: number = 123;
+numero = 321;
+
+let deuBom: Boolean = false;
+deuBom = true;
+
+const fun = (value: string): string => {
+    return value;
+};
+
+console.log(fun('testeeee 4321'));
