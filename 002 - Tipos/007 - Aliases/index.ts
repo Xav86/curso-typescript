93439572844834f6d64431de902881c0de5244f2
Xav86 <gustavo.g.2005@gmail.com>
2024-08-05 14:44:28 -0300

    visto como reutilizar tipos ayphanumericos

---
+++ 002 - Tipos/007 - Aliases/index.ts
@@ -0,0 +1,14 @@
+type alphanumeric = string | number;
+
+let dados: alphanumeric;
+let dados2: alphanumeric;
+
+dados = 1;
+dados = 'G';
+dados = true;
+
+dados2 = 1;
+dados2 = 'G';
+dados2 = true;
+
+console.log(dados, dados2);
