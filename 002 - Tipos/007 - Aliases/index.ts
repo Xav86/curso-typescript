06387776fab8b28635a66f8b5b86e4ee2ac10991
Xav86 <gustavo.g.2005@gmail.com>
2024-08-05 14:45:25 -0300

    comentado código para não dar erro

--- 002 - Tipos/007 - Aliases/index.ts
+++ 002 - Tipos/007 - Aliases/index.ts
@@ -5,10 +5,10 @@ let dados2: alphanumeric;
 
 dados = 1;
 dados = 'G';
-dados = true;
+// dados = true;
 
 dados2 = 1;
 dados2 = 'G';
-dados2 = true;
+// dados2 = true;
 
 console.log(dados, dados2);
